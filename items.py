fe5e86c56e099bef6c5628b4f0eb2469da40e816
Rennze <rennze.fabe2001@gmail.com>
2025-10-14 21:53:46 +0300

    use CSS and show item info in index

--- items.py
+++ items.py
@@ -53,7 +53,12 @@ def get_classes(item_id):
     return db.query(sql, [item_id])
 
 def get_items():
-    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    sql = """SELECT items.id, items.title, users.id user_id, users.username,
+             COUNT(attendees.id) attendee_count
+             FROM items JOIN users ON items.user_id = users.id
+                        LEFT JOIN attendees ON items.id = attendees.item_id
+             GROUP BY items.id
+             ORDER BY items.id DESC"""
     return db.query(sql)
 
 def get_item(item_id):
